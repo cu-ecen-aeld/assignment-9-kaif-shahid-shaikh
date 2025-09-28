d70febfa63fdd6f74f851f2e70e98a5dd213fd0b
kaif-shahid-shaikh <kash7211@colorado.edu>
2025-09-28 05:04:55 -0600

    A5: build aesdsocket from A3 via SSH; pin to 9b925c7 (SUBDIR=server)

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -2,33 +2,24 @@
 # AESD-ASSIGNMENTS (A5)
 ##############################################################
 
-# Build the server from your Assignment 3+ repo.
-# NOTE: This repo MUST contain a top-level "server/" directory.
-# Uses SSH URL as required by the grader.
+# Pin to the A3 commit that passed native tests
+AESD_ASSIGNMENTS_VERSION = 9b925c7
 
-AESD_ASSIGNMENTS_VERSION = HEAD
+# Fetch via SSH (required by grader)
+AESD_ASSIGNMENTS_SITE        = git@github.com:cu-ecen-aeld/assignment-3-kaif-shahid-shaikh.git
+AESD_ASSIGNMENTS_SITE_METHOD = git
 
-# Optional local override for development:
-#   export AESD_ASSIGNMENTS_OVERRIDE_SRCDIR=~/assignment-3-kaif-shahid-shaikh
-ifeq ($(strip $(AESD_ASSIGNMENTS_OVERRIDE_SRCDIR)),)
-  AESD_ASSIGNMENTS_SITE        = git@github.com:cu-ecen-aeld/assignment-3-kaif-shahid-shaikh.git
-  AESD_ASSIGNMENTS_SITE_METHOD = git
-else
-  AESD_ASSIGNMENTS_SITE        = $(AESD_ASSIGNMENTS_OVERRIDE_SRCDIR)
-  AESD_ASSIGNMENTS_SITE_METHOD = local
-endif
-
-# If your repo has submodules, uncomment this:
-# AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+# Build only the server subdirectory
+AESD_ASSIGNMENTS_SUBDIR      = server
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/server clean
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/server CROSS_COMPILE="$(TARGET_CROSS)"
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) clean
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) CROSS_COMPILE="$(TARGET_CROSS)"
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
-	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -D -m 0755 $(@D)/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -D -m 0755 $(@D)/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
