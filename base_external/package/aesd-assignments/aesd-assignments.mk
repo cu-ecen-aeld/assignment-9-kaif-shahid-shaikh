5bc1fde09868c01fd9e79363e137d5c250d7bfff
kaif-shahid-shaikh <kash7211@colorado.edu>
2025-09-28 14:07:39 -0600

    A5: build server only; remove finder-app installs

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -3,21 +3,23 @@
 ##############################################################
 
 # Pin to the A3 commit that passed native tests
-AESD_ASSIGNMENTS_VERSION = NEW_SHA
-
-# Fetch via SSH (required by grader)
 AESD_ASSIGNMENTS_SITE        = git@github.com:cu-ecen-aeld/assignment-3-kaif-shahid-shaikh.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
+AESD_ASSIGNMENTS_VERSION = $(shell git ls-remote $(AESD_ASSIGNMENTS_SITE) master | cut -f1)
+AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/server clean
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/server CROSS_COMPILE="$(TARGET_CROSS)"
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server clean
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
-	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+
+    $(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+    $(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
+
 
