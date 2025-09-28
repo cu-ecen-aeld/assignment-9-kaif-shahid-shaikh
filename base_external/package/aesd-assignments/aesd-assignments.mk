f0af2bcbddf137c701e19ec078c70c81c2ced3ff
kaif-shahid-shaikh <kash7211@colorado.edu>
2025-09-28 04:17:00 -0600

    A5: build aesdsocket from SSH git repo; remove finder-app build

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,40 +1,34 @@
 ##############################################################
-# AESD-ASSIGNMENTS
+# AESD-ASSIGNMENTS (A5)
 ##############################################################
+
+# Build the server from your Assignment 3+ repo.
+# NOTE: This repo MUST contain a top-level "server/" directory.
+# Uses SSH URL as required by the grader.
 
 AESD_ASSIGNMENTS_VERSION = HEAD
 
-# Default: fetch via git over SSH (required by grader)
-AESD_ASSIGNMENTS_SITE        = git@github.com:cu-ecen-aeld/assignment-3-kaif-shahid-shaikh.git
-AESD_ASSIGNMENTS_SITE_METHOD = git
-# Enable this only if your repo actually uses submodules
+# Optional local override for development:
+#   export AESD_ASSIGNMENTS_OVERRIDE_SRCDIR=~/assignment-3-kaif-shahid-shaikh
+ifeq ($(strip $(AESD_ASSIGNMENTS_OVERRIDE_SRCDIR)),)
+  AESD_ASSIGNMENTS_SITE        = git@github.com:cu-ecen-aeld/assignment-3-kaif-shahid-shaikh.git
+  AESD_ASSIGNMENTS_SITE_METHOD = git
+else
+  AESD_ASSIGNMENTS_SITE        = $(AESD_ASSIGNMENTS_OVERRIDE_SRCDIR)
+  AESD_ASSIGNMENTS_SITE_METHOD = local
+endif
+
+# If your repo has submodules, uncomment this:
 # AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
-# Optional local override for your VM (never set in CI unless you export it)
-# ifeq ($(strip $(AESD_ASSIGNMENTS_OVERRIDE_SRCDIR)),)
-#   AESD_ASSIGNMENTS_SITE        = git@github.com:cu-ecen-aeld/assignments-3-and-later-kaif-shahid-shaikh.git
-#   AESD_ASSIGNMENTS_SITE_METHOD = git
-# else
-#   AESD_ASSIGNMENTS_SITE        = $(AESD_ASSIGNMENTS_OVERRIDE_SRCDIR)
-#   AESD_ASSIGNMENTS_SITE_METHOD = local
-# endif
-
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/finder-app clean
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/finder-app CROSS_COMPILE="$(TARGET_CROSS)"
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/server clean
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/server CROSS_COMPILE="$(TARGET_CROSS)"
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d $(TARGET_DIR)/etc/finder-app/conf
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
-
